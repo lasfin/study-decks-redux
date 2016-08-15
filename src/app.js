2aa878fac5c3df4620d2d5a147964ef64c53ffbb
Evgenij Kopitsa <e.kopitca@semrush.com>
2016-08-15 19:15:55 +0300

    render some components

---
+++ src/app.js
@@ -0,0 +1,54 @@
+const cards = (state, action) => {
+
+    switch (action.type) {
+        case 'ADD_CARD':
+            let newCard = Object.assign({}, action.data, {
+                score: 1,
+                id: +new Date()
+            });
+
+            return state.concat([newCard]);
+
+        default:
+            return state || [] ;
+    }
+
+};
+
+const store = Redux.createStore(Redux.combineReducers({
+    cards
+}));
+
+
+const App = (props) => {
+  return (<div className="app">
+      {props.children}
+  </div>);
+};
+
+const Sidebar = React.createClass({
+   render() {
+       let props = this.props;
+       return (
+           <div>
+               <h2>All Decks</h2>
+               <ul>
+                   {props.decks.map((deck, i) =>
+                       <li key={i}> {deck.name} </li>
+                   )}
+               </ul>
+               { props.addingDeck && <input ref='add' />}
+           </div>
+       )
+   }
+});
+
+ReactDOM.render((
+        <App>
+            <Sidebar decks={[ { name: 'Deck 1 '} ]} addingDeck={false} />
+        </App>
+    ),
+    document.getElementById('root')
+);
+
+
