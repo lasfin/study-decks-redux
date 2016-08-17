59036484a603d72a0b02e046baa0dd8a040cf1ba
Evgenij Kopitsa <e.kopitca@semrush.com>
2016-08-17 18:24:10 +0300

    adequate file structure

--- src/app.js
+++ src/app.js
@@ -1,97 +1,12 @@
-const addDeck = (name) => ({type: 'ADD_DECK', data: name});
-const showAddDeck = () => ({type: 'SHOW_ADD_DECK'});
-const hideAddDeck = () => ({type: 'HIDE_ADD_DECK'});
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, combineReducers}  from 'redux';
+import {addDeck, showAddDeck, hideAddDeck} from './actions';
+import * as reducers from './reducers';
+import App from './components/App';
+import Sidebar from './components/Sidebar';
 
-const cards = (state, action) => {
-
-    switch (action.type) {
-        case 'ADD_CARD':
-            let newCard = Object.assign({}, action.data, {
-                score: 1,
-                id: +new Date()
-            });
-
-            return state.concat([newCard]);
-
-        default:
-            return state || [] ;
-    }
-
-};
-
-
-const decks = (state, action) => {
-
-    switch (action.type) {
-        case 'ADD_DECK':
-            let newDeck = {
-                name: action.data,
-                id: +new Date
-            };
-            return state.concat([newDeck]);
-        default:
-            return state || [];
-    }
-};
-
-
-const addingDeck = (state, action) => {
-    switch (action.type) {
-        case 'SHOW_ADD_DECK': return true;
-        case 'HIDE_ADD_DECK': return false;
-        default: return !!state;
-    }
-};
-
-
-const store = Redux.createStore(Redux.combineReducers({
-    cards,
-    decks,
-    addingDeck
-}));
-
-
-const App = (props) => {
-  return (<div className="app">
-      {props.children}
-  </div>);
-};
-
-const Sidebar = React.createClass({
-    componentDidUpdate() {
-        var el = ReactDOM.findDOMNode(this.refs.add);
-        if (el) {
-            el.focus();
-        }
-    },
-
-
-   render() {
-       let props = this.props;
-       return (
-           <div className="sidebar">
-               <h2>All Decks</h2>
-               <button onClick={ e => this.props.showAddDeck()}>New Deck</button>
-               <ul>
-                   {props.decks.map((deck, i) =>
-                       <li key={i}> {deck.name} </li>
-                   )}
-               </ul>
-               { props.addingDeck && <input ref='add' onKeyPress={this.createDeck}/>}
-           </div>
-       )
-   },
-
-   createDeck(event) {
-       if (event.which !== 13) return;
-       var name = ReactDOM.findDOMNode(this.refs.add).value;
-
-       this.props.addDeck(name);
-       this.props.hideAddDeck();
-   }
-});
-
-
+const store = createStore(combineReducers(reducers));
 
 function run() {
     let state = store.getState();
